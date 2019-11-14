4d4d95e09d5f79d3de8430e4abc03be5bf7d67f5
Rogério De Oliveira <rogeriofilho632@gmail.com>
2019-11-14 10:11:01 -0300

    add: User routes

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,18 @@
 import { Router } from 'express';
 import NoticiaController from './app/controllers/NoticiaController';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
+/* Rotas para o controller de Notícias */
 routes.get('/noticia', NoticiaController.show);
 routes.get('/noticias', NoticiaController.index);
-
 routes.post('/noticia/create', NoticiaController.store);
-
 routes.put('/noticia/update', NoticiaController.update);
-
 routes.delete('/noticia/destroy', NoticiaController.delete);
+
+/* Rotas para o controller de Usuários */
+routes.get('/usuarios', UserController.index);
+routes.post('/usuarios/create', UserController.store);
 
 export default routes;
