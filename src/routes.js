6b825bdb955747e80ef610005e9ab2fdfbb28039
Rogério De Oliveira <rogeriofilho632@gmail.com>
2019-11-14 09:50:44 -0300

    add: NoticiaController routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,15 @@
 import { Router } from 'express';
+import NoticiaController from './app/controllers/NoticiaController';
 
 const routes = new Router();
+
+routes.get('/noticia', NoticiaController.show);
+routes.get('/noticias', NoticiaController.index);
+
+routes.post('/noticia/create', NoticiaController.store);
+
+routes.put('/noticia/update', NoticiaController.update);
+
+routes.delete('/noticia/destroy', NoticiaController.delete);
 
 export default routes;
