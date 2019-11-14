3c2777deabc148ae9d8e931775fb2da702aa431b
Rogério De Oliveira <rogeriofilho632@gmail.com>
2019-11-14 10:11:17 -0300

    add: index 'n store methods

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,35 @@
 import User from '../models/User';
 
 class UserController {
-  async store(req, res) {}
+  async index(req, res) {
+    const AllUsers = await User.findAll().then(users => {
+      return users;
+    });
+
+    if (!AllUsers) {
+      return res
+        .status(500)
+        .json({ message: 'Não foi encontrado usuários cadastrados' });
+    }
+    return res.json(AllUsers);
+  }
+
+  async store(req, res) {
+    const { email } = req.body;
+
+    const emailExists = await User.findOne({ where: { email } });
+
+    if (emailExists) {
+      return res.status(400).json({ message: 'This email are already in use' });
+    }
+    const createUser = await User.create(req.body);
+
+    if (!createUser) {
+      return res
+        .status(500)
+        .json({ message: 'Um erro foi encontrado! tente novamente.' });
+    }
+    return res.status(201).json({ message: 'Cadastrado com sucesso!' });
+  }
 }
+export default new UserController();
