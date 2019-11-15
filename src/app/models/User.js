9f3c4661ee6b3056a35c780efadbee0274e9cea0
Rogério De Oliveira <rogeriofilho632@gmail.com>
2019-11-15 17:08:14 -0300

    update: virtual pass 'n password verification

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,16 +1,28 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
     super.init(
       {
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
       },
       {
         sequelize,
       }
     );
+    this.addHook('beforeSave', async usuario => {
+      if (usuario.password) {
+        usuario.password_hash = await bcrypt.hashSync(usuario.password, 8);
+      }
+    });
+    return this;
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
   }
 }
 export default User;
