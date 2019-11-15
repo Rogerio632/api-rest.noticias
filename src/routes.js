497bf766fe032a65da2007e2b833511919bfb44d
Rogério De Oliveira <rogeriofilho632@gmail.com>
2019-11-15 17:07:15 -0300

    add: auth to session route

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import NoticiaController from './app/controllers/NoticiaController';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
+
+/* -- Autenticação de Usuário -- */
+routes.post('/sessions', SessionController.store);
 
 /* Rotas para o controller de Notícias */
 routes.get('/noticia', NoticiaController.show);
@@ -11,7 +15,7 @@ routes.post('/noticia/create', NoticiaController.store);
 routes.put('/noticia/update', NoticiaController.update);
 routes.delete('/noticia/destroy', NoticiaController.delete);
 
-/* Rotas para o controller de Usuários */
+/* -- Rotas para o controller de Usuários -- */
 routes.get('/usuarios', UserController.index);
 routes.post('/usuarios/create', UserController.store);
 
