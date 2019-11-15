92f350c026ab9fbe36779e60c26de3898853e9f2
Rogério De Oliveira <rogeriofilho632@gmail.com>
2019-11-15 17:33:05 -0300

    update:  data validation

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,9 +1,19 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Something is wrong' });
+    }
+
     const { email, password } = req.body;
 
     const usuario = await User.findOne({ where: { email } });
